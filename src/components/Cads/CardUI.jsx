5e29489fffcf4b5ca37033e8ed61402ed9c4a1ef
dipanshu276 <83580591+dipanshu276@users.noreply.github.com>
2021-05-23 15:19:19 +0530

    min

--- src/components/Cads/CardUI.jsx
+++ src/components/Cads/CardUI.jsx
@@ -5,18 +5,15 @@ const Card= props=>{
 return(
     <div className="card text-center shadow ">
         <div className="overflow">
-        <img src={props.imgsrc} alt="Image 1" className="card-img-top"/>
+        <img src={props.imgsrc} alt="Image 1"  className="card-img-top"/>
         </div>
         <div className="card-body text-dark">
-            <h4 className="card-title">{props.title}</h4>
-            <p className="card-text text-secondary" font="Cambria">{props.text}
+            <h4 className="card-title" font="Lucida Console">{props.title}</h4>
+            <p className="card-text text-secondary">{props.text}
             </p>
-            <div class="btn-group dropright">
             <a href={props.link} class="btn btn-outline dropdown-toggle">Read more</a>
             </div>
-
         </div>
-    </div>
 );
 
 }
