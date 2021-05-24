eaefdc05fd9c2a549aba0df6a52a5c62cedd1f3c
Udit singh <Udit-singh@users.noreply.github.com>
2021-05-24 13:31:51 +0530

    fix

--- src/components/Cads/Cards.jsx
+++ src/components/Cads/Cards.jsx
@@ -18,21 +18,21 @@ class Cards extends Component
 <div className="conatiner-fluid d-flex justify-content-center">
     <div className="col">
     <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card  imgsrc={img1} title="SMASHES" text="Ravindra Jadeja Introduces Fans To His ' 22 twenty Acres Entertainer', Michael Vaughan Reacts.See Pics" link="https://sports.ndtv.com/cricket/ravindra-jadeja-introduces-fans-his-22-acres-entertainer-michael-vaughan-reacts-2442474" />
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card imgsrc={img2} title="STATEMENT BY JADDU" text="Should Try With The Bat: Ravindra Jadeja On MS Dhoni's Sword Wielding Gesture. Watch" link="https://sports.ndtv.com/cricket/ms-dhoni-does-ravindra-jadejas-trademark-sword-wielding-gesture-csk-all-rounder-reacts-watch-2442951"/>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card imgsrc={img3} title="HITS BACK" text="Lot Of People To Blame: Former Australia Bowling Coach David Sekar On Sandpaper Gate" link="https://sports.ndtv.com/cricket/lot-of-people-to-blame-former-australia-bowling-coach-david-sekar-on-sandpaper-gate-2442896"/>
         </div>
     </div>
     <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card  imgsrc={img4} title="BEST IN WORLD" text="Virat Kohli Best Batsman In The World, Will Always Remember Him, Says Tim Paine" link="https://sports.ndtv.com/cricket/virat-kohli-best-batsman-in-the-world-will-always-remember-him-says-tim-paine-2442848"/>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card imgsrc={img5} title="WOMENS'S CRICKET" text="Indian Women's Cricket Team Likely To Tour Australia In September, Hints Megan Schutt" link="https://sports.ndtv.com/cricket/indian-womens-cricket-team-likely-to-tour-australia-in-september-hints-megan-schutt-2442835"/>
         </div>
         <div className="col-md-4">
@@ -40,13 +40,13 @@ class Cards extends Component
         </div>
     </div>
     <div className="row">
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card  imgsrc={img7} title="RAJENDRASINH JADEJA" text="Former Saurashtra Cricketer And BCCI Referee Rajendrasinh Jadeja Dies Of COVID-19" link="https://sports.ndtv.com/cricket/rajendrasinh-jadeja-former-saurashtra-cricketer-and-bcci-referee-dies-2442661"/>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card imgsrc={img8} title="RECOVERING FASTLY" text="I Wake Up Each Day Stronger: T Natarajan Recovering From Knee Surgery" link="https://sports.ndtv.com/cricket/t-natarajan-recovering-from-knee-surgery-says-i-wake-up-each-day-stronger-2442702"/>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-4 gap">
             <Card imgsrc={img9} title="HARDIK'S BOY" text="Watch:Baby Pandya On The Move, Mom Natasa Stankovic And Dad Hardik Pandya Can't Stop Smiling" link="https://sports.ndtv.com/cricket/watch-baby-pandya-on-the-move-mom-natasa-stankovic-and-dad-hardik-pandya-cant-stop-smiling-2442752"/>
         </div>
     </div>
